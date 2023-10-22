296b68cd34c133b9427635dd60d841f99af460bb
richardnarta <richardnarta08@gmail.com>
2023-10-22 20:29:39 +0700

    update

--- table/table.js
+++ table/table.js
@@ -1,9 +1,11 @@
-var table = document.getElementById('tabel');
+if(sessionStorage.length > 1){
+    var table = document.getElementById('tabel');
 
-var row = table.insertRow(6);
-const header = ["nama", "nim", "prodi", "email", "pass", "jk", "tl", "alamat"]
+    var row = table.insertRow(6);
+    const header = ["nama", "nim", "prodi", "email", "pass", "jk", "tl", "alamat"]
 
-for(let i=0; i<8; i++){
-    var cell = row.insertCell(i);
-    cell.innerHTML = sessionStorage.getItem(header[i])
+    for(let i=0; i<8; i++){
+        var cell = row.insertCell(i);
+        cell.innerHTML = sessionStorage.getItem(header[i])
+    }
 }
